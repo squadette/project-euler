478b1b2833ac4c549799ff818348b34689525d9a
Alexey Mahotkin <squadette@gmail.com>
2010-02-16 16:07:22 +0300

    Simplify problem #2

--- 002/problem002.hs
+++ 002/problem002.hs
@@ -1,12 +1,8 @@
-import Monad
-import Control.Monad.State
-
 result002 :: Integer -> Integer
-result002 n = sum (filter even (result002' n [2, 1] 1))
-
-result002' n list nextn 
-           | nextn < n = result002' n ((head list + nextn):list) (head list)
-           | otherwise = list
+result002 n = sum (filter even (result002' [2, 1] 1))
+    where result002' list nextn 
+            | nextn < n = result002' ((head list + nextn):list) (head list)
+            | otherwise = list
 
 main :: IO ()
 main = print $ result002 4000000
