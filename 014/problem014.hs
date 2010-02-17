c905775680083e75bdb2e1113a42417ba616afeb
Alexey Mahotkin <squadette@gmail.com>
2010-02-17 16:49:41 +0300

    Перечитываем условие задачи и находим число, а не длину

--- 014/problem014.hs
+++ 014/problem014.hs
@@ -1,6 +1,9 @@
+import Data.List
 import qualified Data.Map as Map
+import Data.Ord
 import Control.Monad.State
 import Data.Maybe
+import System.IO
 
 step = 1000
 
@@ -11,17 +14,22 @@ collatz_len2 n cache = (answer, cache3)
                              answer = 1 + nrest
                              cache3 = Map.insert n answer cache2
 
-collatz_len :: Integer -> State (Map.Map Integer Integer) Integer
+collatz_len :: Integer -> State (Map.Map Integer Integer) (Integer, Integer)
 collatz_len n = get >>= \cache -> case Map.lookup n cache of 
-                                    Just answer -> return answer
+                                    Just answer -> return (n, answer)
                                     Nothing -> let (answer, cache2) = (collatz_len2 n cache)
-                                               in put cache2 >> return answer
+                                               in put cache2 >> return (n, answer)
 
-print_step :: Integer -> IO ()
-print_step n = do
-  putStrLn $ (show n) ++ "\t" ++ (show (maximum (evalState (mapM collatz_len [n..n+step-1]) Map.empty)))
+print_step :: Map.Map Integer Integer -> Integer -> IO ()
+print_step cache 1 = return ()
+print_step cache n = do
+  let (pairs, cache2) = (runState (mapM collatz_len [n,n-1..n-step+1]) cache)
+  let max_pair = maximumBy (comparing snd) pairs
+  putStrLn $ (show (snd max_pair)) ++ "\t" ++ (show (fst max_pair)) ++ "\t" ++ (show $ Map.size cache2)
+  hFlush stdout
+  print_step cache2 (n - step)
 
-result014 n = mapM_ print_step [k | k <- [1,step+1..n]]
+result014 n = print_step Map.empty (n + 1)
 
 main :: IO ()
 main = do
