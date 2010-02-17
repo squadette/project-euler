1ef33b1fe0773c53f8cbbac06ee429137e2a08af
Alexey Mahotkin <squadette@gmail.com>
2010-02-17 15:23:37 +0300

    Пошагово показываем результаты

--- 014/problem014.hs
+++ 014/problem014.hs
@@ -2,6 +2,8 @@ import qualified Data.Map as Map
 import Control.Monad.State
 import Data.Maybe
 
+step = 1000
+
 collatz_len2 :: Integer -> Map.Map Integer Integer -> (Integer, Map.Map Integer Integer)
 collatz_len2 1 cache = (1, cache)
 collatz_len2 n cache = (answer, cache3)
@@ -10,14 +12,18 @@ collatz_len2 n cache = (answer, cache3)
                              cache3 = Map.insert n answer cache2
 
 collatz_len :: Integer -> State (Map.Map Integer Integer) Integer
-collatz_len 1 = return 1
-collatz_len n = do
-  get >>= \cache -> let (ans, cache2) = case Map.lookup n cache of
-                                          Just answer -> (answer, cache)
-                                          Nothing -> (collatz_len2 n cache)
-                    in put cache2 >> return ans
+collatz_len n = get >>= \cache -> let (ans, cache2) = case Map.lookup n cache of
+                                                        Just answer -> (answer, cache)
+                                                        Nothing -> (collatz_len2 n cache)
+                                  in put cache2 >> return ans
 
-result014 n = maximum (evalState (mapM collatz_len [1..n]) Map.empty)
+print_step :: Integer -> IO ()
+print_step n = do
+  putStrLn (show n)
+  putStrLn (show (maximum (evalState (mapM collatz_len [n..n+step-1]) Map.empty)))
+
+result014 n = mapM_ print_step [k | k <- [1,step+1..n]]
 
 main :: IO ()
-main = print $ result014 1000000
+main = do
+  result014 1000000
